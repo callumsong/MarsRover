2c778afaed4f789c65a97a7e938fa84e3bccca5f
callumsong <ghksong@gmail.com>
2015-06-04 10:26:35 +0100

    rover roams freely with no constraints. constraints need to be put in place

--- api/rover.js
+++ api/rover.js
@@ -4,15 +4,15 @@ function Rover (position) {
 
 Rover.prototype.move = function(position, direction) {
   var compass = {
-    'N': 1,
-    'S': 1,
-    'E': 0,
-    'W': 0
+    'N': [1, 1],
+    'S': [1, -1],
+    'E': [0, 1],
+    'W': [0, -1]
   };
   if (direction === 'F') {
-    position[compass[position[2]]] += 1;
+    position[compass[position[2]][0]] += compass[position[2]][1];
   } else if (direction === 'B') {
-    position[compass[position[2]]] -= 1;
+    position[compass[position[2]][0]] -= compass[position[2]][1];
   }
 
   return position;
@@ -40,6 +40,17 @@ Rover.prototype.turn = function(position, direction) {
   }
 
   return position;
+}
+
+Rover.prototype.roam = function(directions) {
+  for (var i = 0; i < directions.length; i++) {
+    if (directions[i] === 'F' || directions[i] === 'B') {
+      this.move(this.position, directions[i]);
+    } else if (directions[i] === 'L' || directions[i] === 'R') {
+      this.turn(this.position, directions[i]);
+    }
+  }
+  return this.position;
 }
 
 module.exports = Rover;
--- spec/test/marsSpec.js
+++ spec/test/marsSpec.js
@@ -29,3 +29,25 @@ describe("Turning", function() {
     expect(rover.turn(rover.position, 'R')).toEqual([1,1,'E']);
   });
 });
+
+describe("Roaming", function() {
+  beforeEach(function() {
+    rover = new Rover([1,1,'N']);
+  });
+
+  it("should be able to take two steps east and two steps north", function() {
+    expect(rover.roam('RFFLFF')).toEqual([3,3,'N']);
+  });
+
+  it("should be able to take two steps north and one step west", function() {
+    expect(rover.roam('FFLF')).toEqual([0,3,'W']);
+  });
+
+  it("should be able to go three steps east and three steps west", function() {
+    expect(rover.roam('RFFFLLFFF')).toEqual([1,1,'W']);
+  });
+
+  it("should be able to go two steps north and two steps south", function() {
+    expect(rover.roam('FFRRFF')).toEqual([1,1,'S']);
+  });
+});
